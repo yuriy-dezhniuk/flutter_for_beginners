83f17b9dafd00a198d936cc8d3123f59aa36b9eb
dezhny2 <yurii.dezhniuk@medtronic.com>
2022-10-03 09:05:17 +0300

    hello world app, first commit

---
+++ android/app/src/main/kotlin/com/dezhniuk/flutter_app/flutter_for_beginners/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dezhniuk.flutter_app.flutter_for_beginners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Hello World App")),
+      ),
+      body: Center(child: Text("Hello World")),
+    ),
+  ));
+}
